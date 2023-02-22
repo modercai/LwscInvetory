1e9ba9d318dd9b120f308be957fd1733cd3de634
modercai <malatefriday12@gmail.com>
2023-02-22 09:09:16 +0200

    Debug false and Allowed Hosts *

--- LwscInvetory/settings.py
+++ LwscInvetory/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3@4=a4*0q8hy0_dth$f0t-w@&-mmv*3omv#of-cmr&*95hx!w0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
